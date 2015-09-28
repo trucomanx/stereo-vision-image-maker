adfbfb0c4e1e7b21ec5cbf84392f592b40655791
TrucomanX <fernando.pujaico.rivera@gmail.com>
2015-09-28 15:44:38 -0300

    agregando datos al modelo

--- script/stereo-tabuleiro2/tabuleiro_left.sh
+++ script/stereo-tabuleiro2/tabuleiro_left.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+#BMP
+#Output_File_Type=S
+
 mkdir tabuleiro_left
 
 for num in -5 -4 -3 -2 -1 0 1 2 3 4 5
 do
-    povray Output_File_Name=tabuleiro_left/tabuleiro_left$num.png Input_File_Name=tabuleiro_left.pov +K$num
+    povray	Output_File_Name=tabuleiro_left/tabuleiro_left$num.png \
+			Input_File_Name=tabuleiro_left.pov \
+			+K$num \
+			Output_File_Type=N \
+			+W400 +H300
 done
-
 
 
--- script/stereo-tabuleiro3/tabuleiro_right.sh
+++ script/stereo-tabuleiro3/tabuleiro_right.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+#BMP
+#Output_File_Type=S
+
 mkdir tabuleiro_right
 
 for num in -5 -4 -3 -2 -1 0 1 2 3 4 5
 do
-	povray Output_File_Name=tabuleiro_right/tabuleiro_right$num.png Input_File_Name=tabuleiro_right.pov +K$num
+	povray	Output_File_Name=tabuleiro_right/tabuleiro_right$num.png \
+			Input_File_Name=tabuleiro_right.pov \
+			+K$num \
+			Output_File_Type=N \
+			+W400 +H300
 done
